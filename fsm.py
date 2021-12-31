a2136a667fb1ace09117306b715bc0ebe358c875
Jansansnss <jansansnss@gmail.com>
2022-01-01 00:32:50 +0800

    united_state test from state2 self.go_back(event)

--- fsm.py
+++ fsm.py
@@ -21,17 +21,15 @@ class TocMachine(GraphMachine):
     #
     def on_enter_state1(self, event):
         print("I'm entering state1")
-
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger state1")
-        self.go_back()
+        self.go_back(event)
 
     def on_exit_state1(self):
         print("Leaving state1")
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
-
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger state2")
         #self.go_back()
@@ -43,10 +41,9 @@ class TocMachine(GraphMachine):
 
     def on_enter_united_state(self, event):
         print("I'm entering united_state")
-
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger united_state")
-        self.go_back()
+        self.go_back(event)
 
     def on_exit_united_state(self):
         print("Leaving united_state")
