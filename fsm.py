d2ef3e6fb97dc81ef812ea86578a2082a1474137
Jansansnss <jansansnss@gmail.com>
2022-01-02 02:11:20 +0800

    will state2 exec print_data?

--- fsm.py
+++ fsm.py
@@ -35,6 +35,7 @@ class TocMachine(GraphMachine):
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
+        print_data('pokemon','*')
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger state2")
         #self.go_back()
